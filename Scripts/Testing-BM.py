03679f04df0ce505de5e52bde9dba1f3f9ef8dbb
Amaretto-Coke <brandonmuise1@gmail.com>
2019-10-03 21:00:27 -0600

    Tinkering with interface.

--- Scripts/Testing-BM.py
+++ Scripts/Testing-BM.py
@@ -1,13 +1,19 @@
-from Scripts.SpatialFunctions import *
+import pandas as pd
+import os
+import math
 
-x = density(mass=4, volume=1)
-print(x)
-print('\n')
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
-x = density(mass=4, volume=0)
-print(x)
-print('\n')
+cases = pd.read_excel(os.path.dirname(os.getcwd()) + r'\MailBox.xlsm', sheet_name='Cases')
+fluid_properties = pd.read_excel(os.path.dirname(os.getcwd()) + r'\MailBox.xlsm', sheet_name='Fluid Properties')
 
-x = density(mass=0, volume=1)
-print(x)
-print('\n')
+# Deleting the units from the column headers
+cases.columns = [col[0:col.find('[')] for col in cases.columns]
+
+cases['CylinderVolume'] = math.pi * (cases.TankID / 2) ** 2 * cases.TankHeight
+cases['FluidVolume'] = math.pi * (cases.TankID / 2) ** 2 * cases.FluidLevel
+
+print(cases)
+
