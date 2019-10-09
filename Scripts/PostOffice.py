4a09c7a3b12272c0b110d8096ac8e63645917357
Amaretto-Coke <brandonmuise1@gmail.com>
2019-10-09 13:26:24 -0600

    Creating import/export functionality 1

---
+++ Scripts/PostOffice.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import os
+import math
+
+
+def import_cases_and_fluids():
+    # Creating a path string to the user interface excel file
+    path = os.path.dirname(os.getcwd()) + r'\MailBox.xlsm'
+
+    # Importing the two Excel sheets as two new data frames
+    cases = pd.read_excel(path, sheet_name='Cases')
+    fluid_properties = pd.read_excel(path, sheet_name='FluidProperties')
+
+    # Deleting the units from the column headers
+    cases.columns = [col[0:col.find('[')] for col in cases.columns]
+
+    # Creating two new columns, of the inside cylinder volume and the initial liquid volume
+    cases['CylinderVolume'] = math.pi * (cases.TankID / 2) ** 2 * cases.TankHeight
+    cases['LiquidVolume'] = math.pi * (cases.TankID / 2) ** 2 * cases.FluidLevel
+
+    # Merges the fluid properties into the cases dataframe
+    cases = cases.merge(fluid_properties, on='FluidName', how='left')
+
+    # Clears the now useless fluid properties dataframe
+    del fluid_properties
+
+    # Returns a the merged dataframe
+    return cases
+
+
+def export_results(dfs=one):
+    # Creating a path string to the user interface excel file
+    path = os.path.dirname(os.getcwd()) + r'\MailBox.xlsm'
+
+    # Creating a writer for the pandas to_excel function
+    writer = pd.ExcelWriter(path)
+
+    for df in dfs:
+        df.to_excel(writer, sheet_name=df.name)
+        
+        
+        
+
--- Scripts/Testing-BM.py
+++ Scripts/Testing-BM.py
@@ -1,19 +1,3 @@
-import pandas as pd
-import os
-import math
-
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
-
-cases = pd.read_excel(os.path.dirname(os.getcwd()) + r'\MailBox.xlsm', sheet_name='Cases')
-fluid_properties = pd.read_excel(os.path.dirname(os.getcwd()) + r'\MailBox.xlsm', sheet_name='Fluid Properties')
-
-# Deleting the units from the column headers
-cases.columns = [col[0:col.find('[')] for col in cases.columns]
-
-cases['CylinderVolume'] = math.pi * (cases.TankID / 2) ** 2 * cases.TankHeight
-cases['FluidVolume'] = math.pi * (cases.TankID / 2) ** 2 * cases.FluidLevel
-
-print(cases)
-
