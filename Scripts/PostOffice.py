479f576a3b051728ee1cbd7c92a2589ac05d4151
Amaretto-Coke <brandonmuise1@gmail.com>
2019-10-09 15:56:05 -0600

    Creating import/export functionality 2

--- Scripts/PostOffice.py
+++ Scripts/PostOffice.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import os
 import math
+from string import ascii_letters
+from openpyxl import load_workbook
+
 
 
 def import_cases_and_fluids():
@@ -29,15 +32,120 @@ def import_cases_and_fluids():
 
 
 def export_results(dfs=None):
-    # Creating a path string to the user interface excel file
-    path = os.path.dirname(os.getcwd()) + r'\MailBox.xlsm'
+    if dfs is None:
+        print('No date frames to export.')
+    else:
+        # Creating a path string to the user interface excel file
+        path = os.path.dirname(os.getcwd()) + r'\MailBox.xlsx'
+        for df in dfs:
+            print('df:', df)
+            i =
+            x = get_df_name(df)
+            print('x:', x)
+            y = validate_excel_sheet_name(x)
+            print('y:', y)
+            sheet_name = validate_excel_sheet_name(get_df_name(df))
+            writer = append_df_to_excel(filename=path, sheet_name=sheet_name, df=df, index=False)
+        writer.save()
+        writer.close()
 
-    # Creating a writer for the pandas to_excel function
-    writer = pd.ExcelWriter(path)
 
-    for df in dfs:
-        df.to_excel(writer, sheet_name=df.name)
+def append_df_to_excel(filename, df,
+                       sheet_name='Sheet1',
+                       start_row=None,
+                       truncate_sheet=True,
+                       **to_excel_kwargs):
+    # Taken from https://stackoverflow.com/questions/38074678/append-existing-excel-sheet-with-new-dataframe-using-python-pandas
+    """
+    Append a DataFrame [df] to existing Excel file [filename]
+    into [sheet_name] Sheet.
+    If [filename] doesn't exist, then this function will create it.
+
+    Parameters:
+      filename : File path or existing ExcelWriter
+                 (Example: '/path/to/file.xlsx')
+      df : dataframe to save to workbook
+      sheet_name : Name of sheet which will contain DataFrame.
+                   (default: 'Sheet1')
+      start_row : upper left cell row to dump data frame.
+                 Per default (startrow=None) calculate the last row
+                 in the existing DF and write to the next row...
+      truncate_sheet : truncate (remove and recreate) [sheet_name]
+                       before writing DataFrame to Excel file
+      to_excel_kwargs : arguments which will be passed to `DataFrame.to_excel()`
+                        [can be dictionary]
+
+    Returns: None
+    """
+
+    # ignore [engine] parameter if it was passed
+    if 'engine' in to_excel_kwargs:
+        to_excel_kwargs.pop('engine')
+
+    writer = pd.ExcelWriter(filename, engine='openpyxl')
+
+    # Python 2.x: define [FileNotFoundError] exception if it doesn't exist
+    try:
+        FileNotFoundError
+    except NameError:
+        FileNotFoundError = IOError
+
+    try:
+        # try to open an existing workbook
+        writer.book = load_workbook(filename)
+
+        # get the last row in the existing Excel sheet
+        # if it was not specified explicitly
+        if start_row is None and sheet_name in writer.book.sheetnames:
+            if truncate_sheet:
+                start_row = 0
+            else:
+                start_row = writer.book[sheet_name].max_row
+
+        # truncate sheet
+        if truncate_sheet and sheet_name in writer.book.sheetnames:
+            # index of [sheet_name] sheet
+            idx = writer.book.sheetnames.index(sheet_name)
+            # remove [sheet_name]
+            writer.book.remove(writer.book.worksheets[idx])
+            # create an empty sheet [sheet_name] using old index
+            writer.book.create_sheet(sheet_name, idx)
+
+        # copy existing sheets
+        writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
+    except FileNotFoundError:
+        # file does not exist yet, we will create it
+        pass
+
+    if start_row is None:
+        start_row = 0
+
+    # write out the new sheet
+    df.to_excel(writer, sheet_name=sheet_name, startrow=start_row, **to_excel_kwargs)
+
+    # save the workbook
+    return writer
         
         
 def validate_excel_sheet_name(val_string):
-    not_allowed = ['/', '*', '?', ':', '[', ']', '\\']
+    allowed = ascii_letters + '1' + '2' + '3' + '4' + '5' + '6' + '7' + '8' + '9' + '0' + '-' + '_' + '.'
+    if all(c in allowed for c in val_string):
+        return val_string
+    else:
+        print('Excel sheet name characters must be alphanumeric or "-", "_", or a ".".')
+        print('All not allowed character converted to "-".')
+        for c in val_string:
+            print(c)
+            if c not in allowed:
+                val_string = val_string.replace(c, '-')
+        return val_string
+
+
+def get_df_name(df):
+    name = [x for x in locals() if locals()[x] is df][0]
+    return name
+
+
+
+
+
--- Scripts/Testing-BM.py
+++ Scripts/Testing-BM.py
@@ -1,3 +1,21 @@
+import pandas as pd
+import os
+from PostOffice import *
+
+
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
+
+data = [['chris', 10], ['nick', 15], ['juli', 14]]
+
+df1 = pd.DataFrame(data=data)
+df2 = pd.DataFrame(data=data)
+
+dfs = [df1, df2]
+
+export_results(dfs)
+
+path = os.path.dirname(os.getcwd()) + r'\MailBox.xlsx'
+
+os.startfile(path)
